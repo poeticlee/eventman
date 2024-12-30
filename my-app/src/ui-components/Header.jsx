24878dc53d14ae779b0aa6036ebbc5007cb65008
leye <donpraise2@gmail.com>
2024-12-30 14:59:11 +0100

    fix

--- my-app/src/ui-components/Header.jsx
+++ my-app/src/ui-components/Header.jsx
@@ -5,19 +5,19 @@ export default function Header() {
   const navigate = useNavigate();
 
   const handleSignUp = () => {
-    navigate("/signUp"); // Navigates to the "/signUp" route
+    navigate("/signUp"); 
   };
   const handleLogin = () => {
-    navigate("/login"); // Navigates to the "/signUp" route
+    navigate("/login"); 
   };
   return (
-    <div>
-      <div>
+    <div className="w-screen h-[80px] flex items-center justify-center ">
+      <div className="flex items-center justify-between main"  >
         <img src="/src/assets/Group 740.png" alt="Logo" />
         <div>
-          <button onClick={handleSignUp}> Sign Up</button>
+          <button  onClick={handleSignUp}> Sign Up</button>
 
-          <button onClick={handleLogin}>Log in</button>
+          <button  className="bg-[#6460FF] fon" onClick={handleLogin}>Log in</button>
         </div>
       </div>
     </div>
